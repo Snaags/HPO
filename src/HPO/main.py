03c66b11071618d5f92fbb406de3cd2809cc11ad
Snaags <cjmackinnon97@gmail.com>
2021-09-13 21:30:49 +0100

    moved training loop to function in repsol worker

--- src/HPO/main.py
+++ src/HPO/main.py
@@ -1,5 +1,5 @@
 import HPO.algorithms.Random as _algorithm
-from HPO.workers.TEPS_worker import compute as _worker
+from HPO.workers.repsol_worker import compute as _worker
 from HPO.searchspaces.OOP_config import init_config as _config
 
 config = _config()
