18704b8fadbc1372f5368c47357c56760afca959
Snaags <cjmackinnon97@gmail.com>
2022-04-27 17:28:09 +0100

    drop path function to 2d

--- src/HPO/utils/utils.py
+++ src/HPO/utils/utils.py
@@ -97,11 +97,19 @@ def save(model, model_path):
 def load(model, model_path):
   model.load_state_dict(torch.load(model_path))
 
-
+"""
 def drop_path(x, drop_prob):
   if drop_prob > 0.:
     keep_prob = 1.-drop_prob
-    mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1).bernoulli_(keep_prob))
+    mask = Variable(torch.cuda.FloatTensor(x.size(0),1, 1).bernoulli_(keep_prob))
+    x.div_(keep_prob)
+    x.mul_(mask)
+  return x
+"""
+def drop_path(x, drop_prob):
+  if drop_prob > 0.:
+    keep_prob = 1.-drop_prob
+    mask = Variable(torch.cuda.FloatTensor(x.size(0), 1,1, 1).bernoulli_(keep_prob))
     x.div_(keep_prob)
     x.mul_(mask)
   return x
