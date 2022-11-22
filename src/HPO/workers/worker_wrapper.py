4bc4fe5dcaca3173d024008bf1e37f6d570ab6e6
Snaags <55416240+Snaags@users.noreply.github.com>
2022-11-22 16:50:46 +0000

    Moved wrapper worker function out

---
+++ src/HPO/workers/worker_wrapper.py
@@ -0,0 +1,43 @@
+def __compute( ID, configs , gpus , res   , JSON_CONFIG, compute_func):
+  device = None
+  config = None
+  print("Starting process: {}".format(ID))
+  while not configs.empty():
+    try:
+      if device == None:
+        device = gpus.get(timeout = 10)
+      config = configs.get(timeout = 10)
+    except Empty:
+      if device != None:
+        gpus.put(device)
+      
+    except:
+      torch.cuda.empty_cache()
+      if device != None:
+        gpus.put(device)
+      return
+    if config != None:
+      print("Got Configuration!")
+
+    if device != None:
+      print("Starting config with device: {}".format(device))
+      complete = False
+      crashes = 0
+      acc , rec =  _compute(hyperparameter = config , cuda_device = device,JSON_CONFIG = JSON_CONFIG)
+      while not complete:
+        try:
+          
+          complete = True
+        except:
+          crashes +=1
+          print("Model crash: {} ".format(crashes))
+          time.sleep(60)
+          if crashes == 2:
+            print("Final Crash giving score of zero")
+            acc , rec = 0 , 0 
+            complete = True
+      res.put([config , acc , rec ]) 
+
+  torch.cuda.empty_cache()
+
+
