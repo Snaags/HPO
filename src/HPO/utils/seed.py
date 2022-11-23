f52843ebe66616095bfcd30ce3d3f33b89491694
Snaags <cjmackinnon97@gmail.com>
2022-11-23 16:18:15 +0000

    fixed issues with loading json

--- src/HPO/utils/seed.py
+++ src/HPO/utils/seed.py
@@ -5,14 +5,16 @@ import json
 
 
 def set_seed(JSON_PATH):
-	with open(JSON_PATH, "w") as f:
-		data = json.load(f)
-		if "SEED" in data:
-			seed = data["SEED"]
-		else:
-			seed = random.randint(0,999)
-			data["SEED"] = seed
-			json.dump(data, JSON_PATH)
-		random.seed(seed)
-		torch.manual_seed(seed)
-		np.random.seed(seed)
+    with open(JSON_PATH, "r") as f:
+        data = json.load(f)
+
+    if "SEED" in data:
+            seed = data["SEED"]
+    else:
+            seed = random.randint(0,999)
+            data["SEED"] = seed
+            with open(JSON_PATH, "w") as f:
+                json.dump(data, f)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
